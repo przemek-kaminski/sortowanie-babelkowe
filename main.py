ffa98843b84a759d0865f9f0641b16ed0ef4b706
przemek-kaminski <88503183+przemek-kaminski@users.noreply.github.com>
2021-08-10 17:52:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,30 +5,23 @@ def wypisz_liste(lista_do_wypisania: List[number]):
         serial.write_string(", ")
     serial.write_line("]")
 
-lista = [7, 9, 5, 8, 3, 6]
+moja_lista = [7, 9, 5, 8, 3, 6]
+inna_lista = [1, 4, 5, 7, 2]
 # lista = [1, 2, 3, 4, 5, 6]
 
-cos2 = 0
-czy_byla_zamiana = 0
+def posortuj(lista: List[number]):
+    czy_byla_zamiana = 1
+    while czy_byla_zamiana > 0:
+        czy_byla_zamiana = 0
+        for i in range(lista.length):
+            if lista[i] > lista[i+1]:
+                temp = lista[i]
+                lista[i] = lista[i+1]
+                lista[i+1] = temp
+                czy_byla_zamiana = 1        
 
-def sortowanie():
-    global cos2, czy_byla_zamiana
-    for index in range(lista.length):
-        if lista[cos2] > lista[cos2+1]:
-            temp = lista[cos2]
-            lista[cos2] = lista[cos2+1]
-            lista[cos2+1] = temp
-            czy_byla_zamiana += 1
-        cos2 += 1
+wypisz_liste(moja_lista)
+posortuj(moja_lista)
+wypisz_liste(moja_lista)
 
-wypisz_liste(lista)
 
-while czy_byla_zamiana > 0:
-    sortowanie()
-    wypisz_liste(lista)
-    cos2 = 0
-    czy_byla_zamiana = 0
-
-def posortuje_liste(lista_do_posortowania: List[number]):
-    # Tutaj coś trzeba zrobić
-    pass
